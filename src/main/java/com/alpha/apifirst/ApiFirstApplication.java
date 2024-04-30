fb4601cbfd669bdb25b3a2bfb9d43dbe9f9c8b92
Frederick Frimpong Arthur <frederick.frimpong@amalitech.com>
2024-04-30 23:25:22 +0000

    Initial commit

---
+++ src/main/java/com/alpha/apifirst/ApiFirstApplication.java
@@ -0,0 +1,13 @@
+package com.alpha.apifirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFirstApplication.class, args);
+	}
+
+}
